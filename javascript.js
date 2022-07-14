dead8d2c0e53e45bf37ad7a3dc2229aee3dc93cd
Spec0s <edgaras2112@mail.ru>
2022-07-14 19:54:38 +0300

    added more logic small fixes

--- javascript.js
+++ javascript.js
@@ -1,29 +1,40 @@
+//---------------------------------------Game Board----------------------------------------------//
 var gameBoard = (() => {
   var gameboardArr = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
   return {
     gameboardArr
   };
 })();
-
-
-//   var player((player, tag) => {
-//     return {player, tag}
-
-//   })();
-
+//---------------------------------------Game functions---------------------------------------//
 var game = (() => {
   const reset = () => {
     game.handleIncrement(true)
     for (let i = 0; i < boardSquare.length; i++) {
       boardSquare[i].innerHTML = ""
     }
+    console.log("Reset done!")
+  }
+  var btnToStart = document.querySelector(".btn-start-restart")
+  var display = document.querySelector(".display-h2")
+  var start = false
 
-    console.log("done reset")
-
+  const gameStart = () => {
+    if (start == false) {
+      start = true
+      display.innerHTML = "Game start"
+      btnToStart.innerHTML = "Reset"
+      return game.start = true
+    } else if (start == true) {
+      game.reset()
+      btnToStart.innerHTML = "Start"
+      display.innerHTML = "Game reset. Press start"
+      start = false
+      return game.start = false
+    }
   }
 
   const checkGame = (mark) => {
-    console.log(mark)
+
     if ((boardSquare[0].innerHTML === mark && boardSquare[1].innerHTML === mark && boardSquare[2].innerHTML === mark)
       ||
       (boardSquare[3].innerHTML === mark && boardSquare[4].innerHTML === mark && boardSquare[5].innerHTML === mark)
@@ -39,13 +50,13 @@ var game = (() => {
       (boardSquare[0].innerHTML === mark && boardSquare[4].innerHTML === mark && boardSquare[8].innerHTML === mark)
       ||
       (boardSquare[2].innerHTML === mark && boardSquare[4].innerHTML === mark && boardSquare[6].innerHTML === mark)) {
-
-      console.log(`${mark} player won!`)
+      display.innerHTML = `${mark} player won!`
       game.reset()
     }
-
+    // else if (game.count == 0) {
+    //   display.innerHTML = "Game start"
+    // }
   };
-
 
   var count = 0
   const handleIncrement = (reset) => {
@@ -55,29 +66,22 @@ var game = (() => {
     } else if (reset === false) {
       return game.count = count++
     }
-
   };
 
   return {
-    reset, checkGame, handleIncrement, count
+    reset, checkGame, handleIncrement, gameStart, count
   }
 })();
-
-
-
+//---------------------------------------on click function---------------------------------------//
 var boardSquare = document.querySelectorAll(".board-div>div")
 boardSquare.forEach(square => {
   square.addEventListener("click", function (e) {
-
     var square = e.target;
-
     if (square.innerHTML != "") {
       return
-    } else {
+    } else if (game.start == true) {
       game.handleIncrement(false)
-
       square.innerHTML = gameBoard.gameboardArr[game.count]
-
       game.checkGame("O")
       game.checkGame("X")
       console.log(game.count)
@@ -86,11 +90,15 @@ boardSquare.forEach(square => {
         game.reset()
       }
     }
-
   })
 })
 
 
+
+//   var player((player, tag) => {
+//     return {player, tag}
+
+//   })();
 
 
 
